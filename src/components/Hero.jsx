80e8de6a24e332d6106311779b04a21811c021f6
subham <subhamdevelopment@gmail.com>
2025-01-29 22:37:54 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,14 +16,14 @@ const Hero = () => {
 
         <div className='flex flex-row justify-between items-center w-full'>
           <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
-            The Next <br className='sm:block hidden'/> {' '} <span className='text-gradient'>Generation</span><br/> {' '}
+            Your Vision <br className='sm:block hidden'/> {' '} <span className='text-gradient'>Our Expertise</span><br/> {' '}
           </h1>
           <div className='ss:flex hidden md:mr-4 mr-0'>
             <GetStarted/>
           </div>
         </div>
 
-        <h1  className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>Payment Method.</h1>
+        <h1  className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>One Step Solution.</h1>
         <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5'>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
        </div>
        <div className='flex-1 flex justify-center items-center md:my-0 my-10 relative'>
