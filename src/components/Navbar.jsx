310d3867c0b8c85db3db4ee084a45a3f07c30e7a
subham <subhamdevelopment@gmail.com>
2025-01-29 23:47:59 +0530

    try

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
-import {close,logo,menu} from '../assets';
+import { close, avix, menu } from '../assets'; // Updated to import avix
 import { navLinks } from '../constant';
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
+      <img src={avix} alt="Avix Ventures" className='w-[124px] h-[32px]' /> {/* Updated to use avix */}
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-        {navLinks.map((nav,index) =>(
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'}`}>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
@@ -19,12 +19,12 @@ const Navbar = () => {
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close : menu} alt="menu" className='w-[28px h-[28px] object-contain' onClick={()=>setToggle((prev) => !prev)}/>
+        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => setToggle((prev) => !prev)} />
 
-        <div className={`${toggle ? 'flex': 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
           <ul className='list-none flex flex-col justify-end items-center flex-1'>
-            {navLinks.map((nav,index) =>(
-              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mb-4'}`}>
+            {navLinks.map((nav, index) => (
+              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'}`}>
                 <a href={`#${nav.id}`}>
                   {nav.title}
                 </a>
@@ -34,7 +34,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  ) 
+  );
 }
 
-export default Navbar
+export default Navbar;
